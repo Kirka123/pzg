3e3bb8c3b94c5835941d3f6187db0acdc13ab5af
Galieva Irina <Galieva.irinka@list.ru>
2017-12-21 10:48:38 +0300

    plusminus2

--- funcs.cpp
+++ funcs.cpp
@@ -1,1 +1,14 @@
 //funcs.cpp
+#include "funcs.h"
+#include <cmath>
+
+double MySum(double a, double b)
+{
+return a + b;
+}
+
+double MySub(double a, double b) 
+{  
+	return a - b;
+} 
+ 
--- funcs.h
+++ funcs.h
@@ -1,1 +1,8 @@
 //funcs.h
+
+#ifndef __FUNCS_H__
+#define __FUNCS_H__
+
+double MySum(double, double);
+double MySub(double, double); 
+#endif // __FUNCS_H__
