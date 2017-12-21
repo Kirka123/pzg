e8d9a609be35a2e8bd3e3012af1b38252b624488
Galieva Irina <Galieva.irinka@list.ru>
2017-12-21 09:52:09 +0300

    slozheniye

--- funcs.cpp
+++ funcs.cpp
@@ -2,4 +2,8 @@
 #include "funcs.h"
 #include <cmath>
 
+double MySum(double a, double b)
+{
+return a + b;
+}
 
--- funcs.h
+++ funcs.h
@@ -3,4 +3,6 @@
 #ifndef __FUNCS_H__
 #define __FUNCS_H__
 
+double MySum(double, double);
+
 #endif // __FUNCS_H__
